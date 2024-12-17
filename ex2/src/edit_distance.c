015b4e3940ff0a764ee3da83294bf528932543f5
“Filippo <filippo.guanti@di.unito.it>
2024-12-17 17:38:33 +0100

    (EX2) Aggiunta librerie, implementazione funzione edit_distance_dyn

--- ex2/src/edit_distance.c
+++ ex2/src/edit_distance.c
@@ -1,28 +1,86 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * Header file containing the definition of INT_MAX, which represents 
+ * the maximum value for a signed 32-bit integer.
+ */
+#include <limits.h>
+
 #include "edit_distance.h"
 
-char *rest(const char *string);
-
+const char *rest(const char *string) {
+    return string + 1;
+}
 int edit_distance(const char *s1, const char* s2){
-    int d_no_op;
-    if(strlen(s1) == 0){
-        return strlen(s2);
-    } else if(strlen(s2) == 0){
-        return strlen(s1);
-    } else{
-        if(*s1 == *s2){
+    if (s1 == NULL || s2 == NULL) return -1; // Controllo iniziale per puntatori NULL
+
+    int len1 = strlen(s1), len2 = strlen(s2);
+    if (len1 == 0) return len2;
+    if (len2 == 0) return len1;
+
+    if(*s1 == *s2){
             d_no_op = edit_distance(rest(s1), rest(s2));
         } else{
             d_no_op = INT_MAX;
         }
-        int d_canc = 1 + edit_distance(s1, rest(s2));
-        int d_ins = 1 + edit_distance(rest(s1), s2);
+    int d_canc = 1 + edit_distance(s1, rest(s2));
+    int d_ins = 1 + edit_distance(rest(s1), s2);
 
-        if (d_canc < d_ins && d_canc < d_no_op) {
+    if (d_canc < d_ins && d_canc < d_no_op) {
         return d_canc;
         } else if (d_ins < d_no_op) {
         return d_ins;
         } else {
         return d_no_op;
         }
-    }    
+ 
+}
+
+int recursive_edit_distance_dyn(const char *s1, const char *s2, int len1, int len2, int **cost_matrix) {
+    return len1 == 0 ? len2 : (len2 == 0 ? len1 : -1);
+    
+    if (cost_matrix[len1][len2] != -1) {
+        return cost_matrix[len1][len2];
+    }
+
+    int d_no_op;
+    if (*s1 == *s2) {
+        d_no_op = recursive_edit_distance_dyn(rest(s1), rest(s2), len1 - 1, len2 - 1, cost_matrix);
+    } else {
+        d_no_op = INT_MAX;
+    }
+
+    int d_canc = 1 + recursive_edit_distance_dyn(s1, rest(s2), len1, len2 - 1, cost_matrix);
+    int d_ins = 1 + recursive_edit_distance_dyn(rest(s1), s2, len1 - 1, len2, cost_matrix);
+
+    int result = (d_canc < d_ins && d_canc < d_no_op) ? d_canc :
+                 (d_ins < d_no_op) ? d_ins : d_no_op;
+
+    cost_matrix[len1][len2] = result;
+    return result;
+}
+
+int edit_distance_dyn(const char *s1, const char *s2) {
+    int len1 = strlen(s1);
+    int len2 = strlen(s2);
+    
+    int **cost_matrix = (int **)malloc((len1 + 1) * sizeof(int *));
+    
+    for (int i = 0; i <= len1; i++) {
+        cost_matrix[i] = (int *)malloc((len2 + 1) * sizeof(int));
+        for (int j = 0; j <= len2; j++) {
+            cost_matrix[i][j] = -1;
+        }
+    }
+    
+    int distance = recursive_edit_distance_dyn(s1, s2, len1, len2, cost_matrix);
+    
+    for (int i = 0; i <= len1; i++) {
+        free(cost_matrix[i]);
+    }
+    free(cost_matrix);
+    
+    return distance;
 }
--- ex2/src/main_ex2.c
+++ ex2/src/main_ex2.c
@@ -1,6 +1,9 @@
 #include "edit_distance.h"
 
 int main(int argc, char *argv[]) {
-    printf("%d", edit_distance("casa", "cassa"));
+    const char *s1 = "tassa";
+    const char *s2 = "passato";
+    int distance = edit_distance_dyn(s1, s2);
+    printf("Edit distance between '%s' and '%s' is %d\n", str1, str2, distance);
     return 0;
 }
