76c4b81eca41a9ad32e5b9d704981e65fb23c372
“Filippo <filippo.guanti@di.unito.it>
2024-12-17 19:09:31 +0100

    (EX2) Correzione errori

--- ex2/src/edit_distance.c
+++ ex2/src/edit_distance.c
@@ -14,17 +14,16 @@ const char *rest(const char *string) {
     return string + 1;
 }
 int edit_distance(const char *s1, const char* s2){
-    if (s1 == NULL || s2 == NULL) return -1; // Controllo iniziale per puntatori NULL
+    if (s1 == NULL || s2 == NULL) return -1;
 
     int len1 = strlen(s1), len2 = strlen(s2);
     if (len1 == 0) return len2;
     if (len2 == 0) return len1;
 
-    if(*s1 == *s2){
+    int d_no_op = INT_MAX;;
+    if(*s1 == *s2)
             d_no_op = edit_distance(rest(s1), rest(s2));
-        } else{
-            d_no_op = INT_MAX;
-        }
+            
     int d_canc = 1 + edit_distance(s1, rest(s2));
     int d_ins = 1 + edit_distance(rest(s1), s2);
 
@@ -35,14 +34,19 @@ int edit_distance(const char *s1, const char* s2){
         } else {
         return d_no_op;
         }
- 
 }
 
-int recursive_edit_distance_dyn(const char *s1, const char *s2, int len1, int len2, int **cost_matrix) {
-    return len1 == 0 ? len2 : (len2 == 0 ? len1 : -1);
-    
+int recursive_edit_distance_dyn(const char *s1, const char *s2, int len1, int len2, int **cost_matrix){
     if (cost_matrix[len1][len2] != -1) {
         return cost_matrix[len1][len2];
+    }
+
+    if (len1 == 0) {
+        cost_matrix[len1][len2] = len2;
+        return len2;
+    } else if (len2 == 0) {
+        cost_matrix[len1][len2] = len1;
+        return len1;
     }
 
     int d_no_op;
@@ -67,7 +71,6 @@ int edit_distance_dyn(const char *s1, const char *s2) {
     int len2 = strlen(s2);
     
     int **cost_matrix = (int **)malloc((len1 + 1) * sizeof(int *));
-    
     for (int i = 0; i <= len1; i++) {
         cost_matrix[i] = (int *)malloc((len2 + 1) * sizeof(int));
         for (int j = 0; j <= len2; j++) {
--- ex2/src/main_ex2.c
+++ ex2/src/main_ex2.c
@@ -1,9 +1,10 @@
+#include <stdio.h>
 #include "edit_distance.h"
 
 int main(int argc, char *argv[]) {
     const char *s1 = "tassa";
     const char *s2 = "passato";
     int distance = edit_distance_dyn(s1, s2);
-    printf("Edit distance between '%s' and '%s' is %d\n", str1, str2, distance);
+    printf("Edit distance between '%s' and '%s' is %d\n", s1, s2, distance);
     return 0;
 }
