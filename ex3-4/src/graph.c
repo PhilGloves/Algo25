a6878a1b8b6ed2e79fc2555bbec097e2b01e8002
Lorenzo Forlano <lorenzo.forlano@edu.unito.it>
2024-12-20 19:13:59 +0100

    correzione errori e gestione errori migliorata

--- ex3-4/src/graph.c
+++ ex3-4/src/graph.c
@@ -31,43 +31,45 @@ Graph graph_create(int labelled, int directed, int (*compare)(const void*, const
 }
 
 int graph_is_directed(const Graph gr) {
-  return gr == NULL ? -1 : gr->directed;
+  return gr->directed;
 }
 
 int graph_is_labelled(const Graph gr) {
-  return gr == NULL ? -1 : gr->labelled;
+  return gr->labelled;
 }
 
 int graph_add_node(Graph gr, const void* node) {
-  if (gr == NULL || node == NULL) {
-    return -1;
+  if (!gr || !node) {
+    perror("Errore nella creazione del nodo");
+    exit(EXIT_FAILURE);
   }
-  if (graph_contains_node(gr, node)) {
-    return 0;
+  if (!hash_table_contains_key(gr->nodes, node)) {
+    hash_table_put(
+        gr->nodes, node,
+        hash_table_create(gr->nodes->compare_keys, gr->nodes->hash_func));
+
+    gr->nodes_size++;
+    return 1;
   }
 
-  hash_table_put(
-      gr->nodes, node,
-      hash_table_create(gr->nodes->compare_keys, gr->nodes->hash_func));
-
-  gr->nodes_size++;
-
-  return 1;
+  return 0;
 }
 
 int graph_add_edge(Graph gr, const void* node1, const void* node2, const void* label) {
-  if (gr == NULL || node1 == NULL || node2 == NULL) {
-    return -1;
+  if (!gr || !node1 || !node2) {
+    perror("Errore nella creazione del arco");
+    exit(EXIT_FAILURE);
   }
 
   if (gr->labelled && label == NULL) {
-    return -1;
+    perror("Etichetta NULL in grafo etichettato");
+    exit(EXIT_FAILURE);
   }
 
-  if (/*!graph_contains_node(gr, node1)*/ !hash_table_get(gr->nodes, node1))
+  if (!hash_table_contains_key(gr->nodes, node1))
     graph_add_node(gr, node1);
 
-  if (/*!graph_contains_node(gr, node2)*/!hash_table_get(gr->nodes, node1))
+  if (!hash_table_contains_key(gr->nodes, node2))
     graph_add_node(gr, node2);
 
   if (graph_contains_edge(gr, node1, node2) == 1) {
@@ -76,42 +78,54 @@ int graph_add_edge(Graph gr, const void* node1, const void* node2, const void* l
 
   EdgeInfo* edge_info = malloc(sizeof(EdgeInfo));
   if (edge_info == NULL) {
-    return -1;
+    perror("Errore allocazione memoria informazioni arco");
+    exit(EXIT_FAILURE);
   }
   edge_info->label = (void*)label;
   edge_info->cost = NULL;
 
   hash_table_put(hash_table_get(gr->nodes, node1), node2, edge_info);
 
-  if (gr->directed) {
-    hash_table_put(hash_table_get(gr->nodes, node2), node1, edge_info);
+  if (!gr->directed) {
+    EdgeInfo* edge_info_copy = malloc(sizeof(EdgeInfo));
+    if (edge_info_copy == NULL) {
+      perror("Errore allocazione memoria informazioni arco");
+      exit(EXIT_FAILURE);
+    }
+    edge_info_copy->label = (void*)label;
+    edge_info_copy->cost = NULL;
+
+    hash_table_put(hash_table_get(gr->nodes, node2), node1, edge_info_copy);
   }
 
   return 1;
 }
 
 int graph_contains_node(const Graph gr, const void* node) {
-  if (gr == NULL || node == NULL) {
-    return -1;
+  if (!gr || !node) {
+    perror("Errore argomenti per contains_node");
+    exit(EXIT_FAILURE);
   }
 
-  return hash_table_get(gr->nodes, node) == NULL ? 0 : 1;
+  return hash_table_contains_key(gr->nodes, node) ? 1 : 0;
 }
 
 int graph_contains_edge(const Graph gr, const void* node1, const void* node2) {
-  if (gr == NULL || node1 == NULL || node2 == NULL) {
-    return -1;
+  if (!gr || !node1 || !node2) {
+    perror("Errore argomenti per contains_edge");
+    exit(EXIT_FAILURE);
   }
 
-  return hash_table_get(hash_table_get(gr->nodes, node1), node2) == NULL ? 0 : 1;
+  return hash_table_contains_key(hash_table_get(gr->nodes, node1), node2)? 1 : 0;
 }
 
 int graph_remove_node(Graph gr, const void* node) {
-  if (gr == NULL || node == NULL) {
-    return -1;
+  if (!gr || !node) {
+    perror("Errore rimozione nodi");
+    exit(EXIT_FAILURE);
   }
 
-  if (!graph_contains_node(gr, node)) {
+  if (!hash_table_contains_key(gr->nodes, node)) {
     return 0;
   }
 
@@ -127,24 +141,25 @@ int graph_remove_node(Graph gr, const void* node) {
   free(keyset);
   hash_table_remove(gr->nodes, node);
 
+  gr->nodes_size--;
+
   //pulisco archi che puntano a nodo
   keyset = hash_table_keyset(gr->nodes);
-  for (size_t i = 0; i < gr->nodes_size; i++) {
+  for (size_t i = 0; i < hash_table_size(gr->nodes); i++) {
     edge_table = hash_table_get(gr->nodes, keyset[i]);
-    if (hash_table_get(edge_table, node) != NULL) {
+    if (hash_table_contains_key(edge_table, node)) {
       free(hash_table_get(edge_table, node));
       hash_table_remove(edge_table, node);
     }
   }
 
-  gr->nodes_size--;
-
   return 1;
 }
 
 int graph_remove_edge(Graph gr, const void* node1, const void* node2) {
-  if (gr == NULL || node1 == NULL || node2 == NULL) {
-    return -1;
+  if (!gr || !node1 || !node2) {
+    perror("Errore rimozione arco");
+    exit(EXIT_FAILURE);
   }
 
   if (!graph_contains_edge(gr, node1, node2)) {
@@ -164,15 +179,13 @@ int graph_remove_edge(Graph gr, const void* node1, const void* node2) {
 }
 
 int graph_num_nodes(const Graph gr) {
-  if (gr == NULL || gr->nodes == NULL) {
-    return -1;
-  }
   return gr->nodes_size;
 }
 
 int graph_num_edges(const Graph gr) {
-  if (gr == NULL) {
-    return -1;
+  if (!gr) {
+    perror("Errore rimozione arco");
+    exit(EXIT_FAILURE);
   }
 
   int edge_count = 0;
@@ -188,22 +201,25 @@ int graph_num_edges(const Graph gr) {
   return edge_count;
 }
 void** graph_get_nodes(const Graph gr) {
-  if (gr == NULL) {
-    return NULL;
+  if (!gr) {
+    perror("Errore get nodi");
+    exit(EXIT_FAILURE);
   }
 
   return hash_table_keyset(gr->nodes);
 }
 
 Edge** graph_get_edges(const Graph gr) {
-  if (gr == NULL) {
-    return NULL;
+  if (!gr) {
+    perror("Errore get archi");
+    exit(EXIT_FAILURE);
   }
 
   Edge** edges = calloc(sizeof(Edge*), graph_num_edges(gr));
 
-  if (edges == NULL) {
-    return NULL;
+  if (!edges) {
+    perror("Errore creazione array archi");
+    exit(EXIT_FAILURE);
   }
 
   size_t index = 0;
@@ -219,10 +235,14 @@ Edge** graph_get_edges(const Graph gr) {
       EdgeInfo* edge_info = hash_table_get(edge_table, edge_keyset[j]);
       Edge* edge = malloc(sizeof(Edge));
       if (edge == NULL) {
-        free(Nodes);
+        for (size_t k = 0; k < index; k++) {
+          free(edges[k]);
+        }
         free(edges);
+        free(Nodes);
         return NULL;
       }
+
       edge->source = Nodes[i];
       edge->dest = edge_keyset[j];
       edge->label = edge_info->label;
@@ -237,23 +257,26 @@ Edge** graph_get_edges(const Graph gr) {
 }
 
 void** graph_get_neighbours(const Graph gr, const void* node) {
-  if (gr == NULL || node == NULL) {
-    return NULL;
+  if (!gr || !node) {
+    perror("Errore get archi");
+    exit(EXIT_FAILURE);
   }
 
   return hash_table_keyset(hash_table_get(gr->nodes, node));
 }
 
 int graph_num_neighbours(const Graph gr, const void* node) {
-  if (gr == NULL || node == NULL) {
-    return -1;
+  if (!gr || !node) {
+    perror("Errore get archi");
+    exit(EXIT_FAILURE);
   }
   return hash_table_size(hash_table_get(gr->nodes, node));
 }
 
 void* graph_get_label(const Graph gr, const void* node1, const void* node2) {
-  if (gr == NULL || node1 == NULL || node2 == NULL) {
-    return NULL;
+  if (!gr || !node1 || !node2) {
+    perror("Errore get archi");
+    exit(EXIT_FAILURE);
   }
   EdgeInfo* info= hash_table_get(hash_table_get(gr->nodes, node1), node2);
   return info->label;
