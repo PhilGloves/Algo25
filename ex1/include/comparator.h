a92b4de099a340c5030d149ca02c6d7f09accbd3
Lorenzo Forlano <lorenzo.forlano@edu.unito.it>
2024-12-30 11:27:05 +0100

    cambiamento gestione unity per ex1 e ex2

--- ex1/include/comparator.h
+++ ex1/include/comparator.h
@@ -20,7 +20,7 @@ int str_compare(const void *a, const void *b);
 * @param b Pointer to the second record
 *
 * @return -1 if a is less than b, 0 if a is equal to b, 1 if a is greater than b
-
+*/
 int int_compare(const void *a, const void *b);
 
 /**
